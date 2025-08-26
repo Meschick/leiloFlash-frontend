646c42c433f602cac56d12b495196146cb807bf0
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-08-26 20:54:16 -0300

    feat: Configurações iniciais do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { Noir } from '../styles';
+import { AuraBaseDesignTokens } from '@primeuix/themes/aura/base';
+import { Preset } from '@primeuix/themes/types';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,31 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Toast,
+    ButtonModule
   ],
-  providers: [],
+   providers: [
+    provideHttpClient(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Noir,
+        options: {
+          darkModeSelector: '.my-app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
+        }
+      }
+    }),
+     provideAnimations(),
+    MessageService,
+    ConfirmationService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
