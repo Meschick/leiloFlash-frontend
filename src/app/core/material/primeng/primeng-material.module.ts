5e8061a825ce1fd7f184d13d3d9568b21d148af3
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-09-07 22:36:01 -0300

    feat: Implementações no componente de header

--- src/app/core/material/primeng/primeng-material.module.ts
+++ src/app/core/material/primeng/primeng-material.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from "@angular/core";
 import { Toast } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
 
 @NgModule({
     imports:[
         Toast,
         ButtonModule,
-        MenubarModule
+        MenubarModule,
+        AvatarModule,
+        OverlayBadgeModule
     ], 
     exports: [
         Toast,
         ButtonModule,
-        MenubarModule
+        MenubarModule,
+        AvatarModule,
+        OverlayBadgeModule
     ]
 })
 export class PrimengMaterialModule {}
