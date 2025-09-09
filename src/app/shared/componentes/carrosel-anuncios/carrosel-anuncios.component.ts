33aefc30da12bcb94143dd9fabb2dc7a84e90926
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-09-08 22:11:21 -0300

    feat: Implementa componente de carrosel de anúncios

---
+++ src/app/shared/componentes/carrosel-anuncios/carrosel-anuncios.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-carrosel-anuncios',
+  templateUrl: './carrosel-anuncios.component.html',
+  styleUrls: ['./carrosel-anuncios.component.scss']
+})
+export class CarroselAnunciosComponent {
+
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1300px',
+      numVisible: 4
+    },
+    {
+      breakpoint: '575px',
+      numVisible: 1
+    }
+  ]
+
+  products: any | undefined;
+
+
+  ngOnInit() {
+    this.dadosMock();
+  }
+
+
+
+  dadosMock() {
+    this.products = [
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        image: 'assets/anuncio3.png'
+      },
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        image: 'assets/anuncio2.png'
+      },
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        image: 'assets/anuncio1.png'
+      },
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        image: 'https://images.pexels.com/photos/5086489/pexels-photo-5086489.jpeg'
+      },
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        image: 'https://images.pexels.com/photos/5086489/pexels-photo-5086489.jpeg'
+      },
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        image: 'https://images.pexels.com/photos/5086489/pexels-photo-5086489.jpeg'
+      },
+
+    ]
+  }
+}
