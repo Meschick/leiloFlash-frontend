f50f821af08e4c25ddba8717d67bb08b12affef8
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-11-16 00:43:47 -0300

    feat: Novas lógicas

--- src/app/shared/componentes/list-leilao-destaque/list-leilao-destaque.component.ts
+++ src/app/shared/componentes/list-leilao-destaque/list-leilao-destaque.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { LoteInterface } from '../../../interfaces/lote.interface';
+import { StatusLeilaoService } from '../../../core/services/status-leilao/status-leilao.service';
 
 interface Lote {
   id: number;
@@ -18,57 +19,48 @@ interface Lote {
   styleUrl: './list-leilao-destaque.component.scss'
 })
 export class ListLeilaoDestaqueComponent implements OnInit, OnChanges {
-
   responsiveOptions!: any[];
+  tempoRestante: string = '';
+  intervalId: any;
+  leilaoAtivo: boolean = true;
+
 
   @Input() nome!: string;
   @Input() dataInicio!: string;
   @Input() dataFim!: string;
   @Input() lotes: LoteInterface[] = [];
 
+
+  constructor(
+    private readonly _statusLeilaoService: StatusLeilaoService
+  ) { }
+
   ngOnInit(): void {
-    this.responsiveOptions = [
-      {
-        breakpoint: '1920px', // telas 1080p ou maiores
-        numVisible: 5,
-        numScroll: 1
-      },
-      {
-        breakpoint: '1600px', // notebooks grandes
-        numVisible: 4,
-        numScroll: 1
-      },
-      {
-        breakpoint: '1366px', // notebooks padrão
-        numVisible: 3,
-        numScroll: 1
-      },
-      {
-        breakpoint: '1024px', // tablets horizontais
-        numVisible: 3,
-        numScroll: 1
-      },
-      {
-        breakpoint: '900px', // tablets verticais
-        numVisible: 2,
-        numScroll: 1
-      },
-      {
-        breakpoint: '768px', // celulares grandes
-        numVisible: 2,
-        numScroll: 1
-      },
-      {
-        breakpoint: '600px', // celulares médios
-        numVisible: 1,
-        numScroll: 1
-      },
-      {
-        breakpoint: '480px', // celulares pequenos
-        numVisible: 1,
-        numScroll: 1
-      }
-    ];
+
+    this.atualizarTempoRestante();
+
+    this.intervalId = setInterval(() => this.atualizarTempoRestante(), 1000);
+
+    this.carroselResponse();
+  }
+
+  atualizarTempoRestante() {
+    const agora = new Date().getTime();
+    const fim = new Date(this.dataFim).getTime();
+    const diff = fim - agora;
+
+    if (diff <= 0) {
+      this.tempoRestante = "Finalizado";
+      this._statusLeilaoService.finalizar();
+      clearInterval(this.intervalId);
+      return;
+    }
+
+    const horas = Math.floor(diff / (1000 * 60 * 60));
+    const minutos = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diff % (1000 * 60)) / 1000);
+
+    this.tempoRestante = `${horas}h ${minutos}m ${segundos}s`;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -77,4 +69,16 @@ export class ListLeilaoDestaqueComponent implements OnInit, OnChanges {
     }
   }
 
+  carroselResponse() {
+    this.responsiveOptions = [
+      { breakpoint: '1920px', numVisible: 5, numScroll: 1 },
+      { breakpoint: '1600px', numVisible: 4, numScroll: 1 },
+      { breakpoint: '1366px', numVisible: 3, numScroll: 1 },
+      { breakpoint: '1024px', numVisible: 3, numScroll: 1 },
+      { breakpoint: '900px', numVisible: 2, numScroll: 1 },
+      { breakpoint: '768px', numVisible: 2, numScroll: 1 },
+      { breakpoint: '600px', numVisible: 1, numScroll: 1 },
+      { breakpoint: '480px', numVisible: 1, numScroll: 1 }
+    ];
+  }
 }
